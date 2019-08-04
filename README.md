924995d9646ea5877debc2e98da453766cb035b4
bartlomiej.guz <bartlomiej.guz@gmail.com>
2019-08-04 17:12:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Recipes
+ Simple recipes iOS app
---
+++ Recipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by bartlomiej.guz on 04/08/2019.
+//  Copyright © 2019 bartlomiej.guz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
